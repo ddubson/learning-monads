afd68577268204db43088590c766400886c2de33
Dmitriy Dubson <ddubson@pivotal.io>
2020-01-02 17:43:59 -0500

    Updated writer monad composer

--- kotlin/src/WriterMonad.kt
+++ kotlin/src/WriterMonad.kt
@@ -5,9 +5,8 @@ data class Writer<T>(val item: T, val message: String)
 fun <T> identity(item: T): Writer<T> = Writer(item, "")
 
 fun main() {
-    val writerComposer = WriterComposer<Int, Int, Int>()
     val f: (Int) -> Writer<Int> = { Writer(it + 5, "value is $it (f morphism called)")}
     val g: (Int) -> Writer<Int> = { Writer(it + 10, "value is now $it (g morphism called)")}
-    val h: (Int) -> Writer<Int> = writerComposer.compose(f, g)
+    val h: (Int) -> Writer<Int> = compose(f, g)
     println(h(4))
 }
