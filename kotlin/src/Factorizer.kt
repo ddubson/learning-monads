26593e0703260db32b837dd101452742c79faa90
Dmitriy Dubson <ddubson@pivotal.io>
2020-01-06 13:02:09 -0500

    Add CoProduct factorizer example

--- kotlin/src/Factorizer.kt
+++ kotlin/src/Factorizer.kt
@@ -1,3 +1,7 @@
+import arrow.core.Either
+import arrow.core.Left
+import arrow.core.Right
+
 /*
     Factorizer higher-order function used to "factorize"
     a product of two objects
@@ -6,10 +10,34 @@
 fun <A, B, C> factorizer(f: (C) -> A, g: (C) -> B): (C) -> Pair<A, B> =
         { Pair(f(it), g(it)) }
 
-fun main() {
+// Inverse of factorizer
+fun <A, B, C> coProductFactorizer(f: (A) -> C, g: (B) -> C): (Either<A, B>) -> C = { eitherOf ->
+    eitherOf.fold(
+            { left -> f(left) },
+            { right -> g(right) }
+    )
+}
+
+fun factorizerExample() {
     val f: (Int) -> Int = { it + 1 }
     val g: (Int) -> Boolean = { true }
 
     val m: (Int) -> Pair<Int, Boolean> = factorizer(f, g)
+    println("Product Factorizer example - creates a Pair")
     println(m(5))
+}
+
+fun coProductFactorizerExample() {
+    val f: (Int) -> (Int) = { it + 5 }
+    val g: (String) -> (Int) = { it.length }
+    val h: (Either<Int, String>) -> (Int) = coProductFactorizer(f, g)
+    println("CoProduct Factorizer example")
+    println("H arrow (left) value is: ${h(Left(10))}")
+    println("H arrow (right) value is: ${h(Right("Some string"))}")
+}
+
+
+fun main() {
+    factorizerExample()
+    coProductFactorizerExample()
 }
