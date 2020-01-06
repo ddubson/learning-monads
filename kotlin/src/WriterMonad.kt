01b262562ae342053bb9d9947d9a33180f16dc2d
Dmitriy Dubson <ddubson@pivotal.io>
2020-01-06 12:48:00 -0500

    Enhance compose example

--- kotlin/src/WriterMonad.kt
+++ kotlin/src/WriterMonad.kt
@@ -7,6 +7,7 @@ fun <T> identity(item: T): Writer<T> = Writer(item, "")
 fun main() {
     val f: (Int) -> Writer<Int> = { Writer(it + 5, "value is $it (f morphism called)")}
     val g: (Int) -> Writer<Int> = { Writer(it + 10, "value is now $it (g morphism called)")}
-    val h: (Int) -> Writer<Int> = compose(f, g)
-    println(h(4))
+    val h: (Int) -> Writer<Int> = { Writer(it + 20, "value is inc by 20")}
+    val k: (Int) -> Writer<Int> = compose(compose(f, g), h)
+    println(k(4))
 }
